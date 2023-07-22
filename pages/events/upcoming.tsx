11a45d46db4a89ebacfe72ca5e93b80f4ebc51b6
Vishwa Shankar <vishwas1020@gmail.com>
2023-07-23 00:20:01 +0400

    update upcoming events

--- pages/events/upcoming.tsx
+++ pages/events/upcoming.tsx
@@ -1,26 +1,44 @@
 import Head from "next/head";
 
-type Event = {
-  date: string; // Change this to the appropriate date format (e.g., "August")
-  title: string;
-};
-
 const events: Event[] = [
-  { date: "August 2023", title: "Journey: Thematic performance by Mathura Viswanathan" },
-  { date: "August 2023", title: "Hasmita's Arangetram" },
-  { date: "August 2023", title: "Sahana's Arangetram" },
-  { date: "August 2023", title: "Samyuktha's Arangetram" },
-  { date: "August 2023", title: "Sadhana's Arangetram" },
+  { date: "08/13/2023", month:"August",year:"2023", title: "Cosmic Elements: Thematic performance by Mathura Viswanathan" },
+  { date: "08/12/2023", month:"August",year:"2023", title: "Arangetram - Hasmitha Vaibhavi Kanugula", poster: '/events/hasmitha-arangetram.jpeg'},
+  { date: "08/19/2023", month:"August",year:"2023", title: "Arangetram - Sahana Shankar" ,poster: '/events/sahana-arangetram.jpeg'},
+  { date: "08/05/2023", month:"August",year:"2023", title: "Arangetram - Samyukta and Sadhana Suresh", poster: '/events/samyukta-arangetram.jpeg' },
 ];
 
+type Event = {
+  date?: string; 
+  month: string;
+  year:string;
+  title: string;
+  poster?:string;
+};
+
 export default function Upcoming() {
-  const eventsByMonth: { [key: string]: string[] } = events.reduce((acc, event) => {
-    if (!acc[event.date]) {
-      acc[event.date] = [];
+  const sortedEvents = events
+    .filter((event) => event.date)
+    .sort((a, b) => new Date(a.date!).getTime() - new Date(b.date!).getTime());
+
+  const eventsByDateAndMonth: { [key: string]: { [key: string]: Event[] } } = sortedEvents.reduce((acc, event) => {
+    const formattedDate = new Date(event.date!).toLocaleDateString('en-US', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    });
+    const month = new Date(event.date!).toLocaleDateString('en-US', {
+      month: 'long',
+      year: 'numeric',
+    });
+    if (!acc[formattedDate]) {
+      acc[formattedDate] = {};
     }
-    acc[event.date].push(event.title);
+    if (!acc[formattedDate][month]) {
+      acc[formattedDate][month] = [];
+    }
+    acc[formattedDate][month].push(event);
     return acc;
-  }, {} as { [key: string]: string[] });
+  }, {} as { [key: string]: { [key: string]: Event[] } });
 
   return (
     <section>
@@ -28,22 +46,35 @@ export default function Upcoming() {
         <title>Upcoming Events - Kaladhaara</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <div className="max-w-2xl mx-auto py-2 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+      <div className="py-2 px-4 sm:py-24 sm:px-6">
         <h1 className="text-2xl md:text-3xl font-semibold text-center mb-4">UPCOMING EVENTS</h1>
-        {Object.entries(eventsByMonth).map(([month, events]) => (
-          <div key={month} className="mb-8">
-            <h2 className="text-2xl font-semibold mb-4">{month}</h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-              {events.map((event) => (
-                <div key={event} className="bg-white p-6 rounded-lg shadow-md">
-                  <p className="text-md text-center md:text-lg font-bold mb-4">{event}</p>
-                  <div className="bg-gray-200 h-px w-1/2 mx-auto"></div>
-                  
-                </div>
-              ))}
+        {Object.entries(eventsByDateAndMonth).map(([formattedDate, eventsByMonth]) => {
+          return (
+            <div key={formattedDate} className="mb-8">
+              <div className="bg-red-800 rounded-lg p-2 mb-1 text-white text-center">
+                <div className="text-4xl font-bold">{new Date(formattedDate).getDate()}</div>
+                <div className="text-2xl font-semibold mt-1">{new Date(formattedDate).toLocaleDateString('en-US', { month: 'long' })}</div>
+                <div className="text-lg mt-1">{new Date(formattedDate).getFullYear()}</div>
+              </div>
+              <div className="grid gap-4">
+                {Object.entries(eventsByMonth).map(([month, events]) => (
+                  <div key={month} className="mb-4">
+                    {events.map((event) => (
+                      <div key={event.title} className="bg-white p-6 rounded-lg shadow-md">
+                        <p className="text-lg text-center md:text-2xl font-semibold mb-4">{event.title}</p>
+                        {event.poster ?  (
+                          <div className="h-auto max-w-[800px] mx-auto">
+                            <img src={event.poster} alt={event.title} className="w-full h-full object-cover rounded-lg" />
+                          </div>
+                        ) : null}
+                      </div>
+                    ))}
+                  </div>
+                ))}
+              </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </section>
   );
