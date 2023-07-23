0d4aa3d90be6897fbf53bf1b5bd14616242a6ef9
Vishwa Shankar <vishwas1020@gmail.com>
2023-07-23 19:57:05 +0400

    fix stretched imgs

--- pages/sac-adavu.tsx
+++ pages/sac-adavu.tsx
@@ -1,39 +1,39 @@
 import Head from "next/head";
 
-export default function Samu(){
+export default function Samu() {
     return (
-        <section>
-            <Head>
-            <title>Sac Adavu - Kaladhaara</title>
-            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-            </Head>
-            <div className="">
-            <h1 className='text-2xl md:text-3xl font-semibold text-center'>SAC ADAVU MEETUP</h1>
-            </div>
-        <p className='mt-8 text-lg text-center leading-8 text-gray-600'>
-            If you have learnt any form of Indian dance years ago and feel the urge to get back in touch and 
-            dance away your stress, look no further. This is what Kaladhaara's newest initiative is all about!
-             SacAdavu meetup is a group of like minded dancers who meet bi monthly on a weekend morning to 
-             revisit the foundation, technique and learn something new every time. The approach is similar 
-             to a drop in dance class.
-        </p>
-        <div className="grid grid-rows-1 md:grid-cols-2 gap-2 mt-8">
-             <div className="aspect-w-2 aspect-h-1">
-                <img src="/sac-adavu/sa1.jpg" className="" />
-              </div>
-              <div className="aspect-w-2 aspect-h-1">
-                <img src="/sac-adavu/sa2.jpg" className="" />
-               </div>
+      <section>
+        <Head>
+          <title>Sac Adavu - Kaladhaara</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
+        <div className="mt-4">
+          <h1 className="text-2xl md:text-3xl font-semibold text-center">SAC ADAVU MEETUP</h1>
         </div>
-        <p className='mt-8 text-lg text-center leading-8 text-gray-600'>
-            This is a collaborative work space that allows for dancers in the 
-            area to learn, grow and enjoy this beautiful art form. Every session starts with a  slow warm up,
-            a few adavus that focuses on form and technique and is followed with a short choreography. 
-            The session concludes with some stretches and relaxed conversations about anything and everything. 
-            If you want to energize your weekend with some “me time”, hop aboard the SacAdavu meetup train!
+        <div className="grid grid-rows-1 gap-2 mt-8">
+          <div className="w-full max-w-[800px] mx-auto">
+            <img src="/sac-adavu/sa1.jpg" className="w-full h-auto rounded-lg" alt="Sac Adavu Meetup" />
+          </div>
+        </div>
+        <p className="mt-8 text-lg text-center leading-8 text-gray-600">
+          If you have learnt any form of Indian dance years ago and feel the urge to get back in touch and
+          dance away your stress, look no further. This is what Kaladhaara's newest initiative is all about!
+          SacAdavu meetup is a group of like-minded dancers who meet bi-monthly on a weekend morning to
+          revisit the foundation, technique and learn something new every time. The approach is similar
+          to a drop-in dance class.
         </p>
-
-        </section>
-        
-    )
-}
+        <div className="grid grid-rows-1 gap-2 mt-8">
+          <div className="w-full max-w-[800px] mx-auto">
+            <img src="/sac-adavu/sa2.jpg" className="w-full h-auto rounded-lg" alt="Sac Adavu Meetup" />
+          </div>
+        </div>
+        <p className="mt-8 text-lg text-center leading-8 text-gray-600">
+          This is a collaborative work space that allows for dancers in the
+          area to learn, grow and enjoy this beautiful art form. Every session starts with a slow warm-up,
+          a few adavus that focus on form and technique and is followed by a short choreography.
+          The session concludes with some stretches and relaxed conversations about anything and everything.
+          If you want to energize your weekend with some “me time”, hop aboard the SacAdavu meetup train!
+        </p>
+      </section>
+    );
+  }
